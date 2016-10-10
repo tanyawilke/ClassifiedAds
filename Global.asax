57ee0d0f98f4be35a120192ed5d52df966d495a1
Tanya <tanya.wilke@gmail.com>
2016-10-10 20:18:19 +0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClassifiedAdsApp.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClassifiedAdsApp.Startup))]
+namespace ClassifiedAdsApp
+{
+    
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
