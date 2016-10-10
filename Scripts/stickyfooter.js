1a09d466fc6545a831f9cf90d9d9863058260972
Tanya <tanya.wilke@gmail.com>
2016-10-10 20:15:38 +0200

    Add files via upload

---
+++ Scripts/stickyfooter.js
@@ -0,0 +1,17 @@
+﻿//$(window).bind("load", function () {
+//    SetFooter();
+//});
+
+//$(window).on("resize", function () {
+//    SetFooter();
+//});
+
+//function SetFooter() {
+//    var winHeight = window.innerHeight;
+//    var contentHeight = document.getElementById("headerNav").clientHeight + document.getElementById("bannerContainer").clientHeight + document.getElementById("contentWrapper").clientHeight + document.getElementById("footer").clientHeight;
+//    var height = winHeight - contentHeight;
+    
+//    if (height > 0) {
+//        $('#footer').addClass('stickybottom');
+//    }
+//}
